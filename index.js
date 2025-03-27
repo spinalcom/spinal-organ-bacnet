1d01e8ccf70a192914416945d686cc9c908f64ef
mbt-tech <mbaradji221@outlook.fr>
2025-03-27 16:03:31 +0100

    fix index.js

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-// require('./dist/index')
+require('./dist/index')
 
 
-const bacnet = require('bacstack');
+// const bacnet = require('bacstack');
 
-// const bacnet = require('node-bacstack');
-// const client = new bacnet({ interface: "192.168.1.66" });
-const client = new bacnet();
+// // const bacnet = require('node-bacstack');
+// // const client = new bacnet({ interface: "192.168.1.66" });
+// const client = new bacnet();
 
 
-client.subscribeCOV('192.168.1.46', { type: 2, instance: 14 }, 1, false, false, 0, (err, data) => {
-    if (err) {
-        console.log('error COV: ', err);
-        return;
-    }
-    console.log('COV: ', data);
-});
-
-// client.subscribeProperty('192.168.1.144', {type: 0, instance: 9015},{id:85, index:4294967295}, 1, false, false, (err) => {
-//     console.log('error COV: ', err);
+// client.subscribeCOV('192.168.1.46', { type: 2, instance: 14 }, 1, false, false, 0, (err, data) => {
+//     if (err) {
+//         console.log('error COV: ', err);
+//         return;
+//     }
+//     console.log('COV: ', data);
 // });
 
+// // client.subscribeProperty('192.168.1.144', {type: 0, instance: 9015},{id:85, index:4294967295}, 1, false, false, (err) => {
+// //     console.log('error COV: ', err);
+// // });
 
-client.on('covNotifyUnconfirmed', (data) => {
-    console.log('Received COV: ' + JSON.stringify(data));
-    console.log(data);
 
-    // console.log(client._events["covNotifyUnconfirmed"].length)
-});
+// client.on('covNotifyUnconfirmed', (data) => {
+//     console.log('Received COV: ' + JSON.stringify(data));
+//     console.log(data);
+
+//     // console.log(client._events["covNotifyUnconfirmed"].length)
+// });
